e6a9c9400f6d57495ab11aa4fe0a9b470675a3b1
Christian Westrom <c.westrom@westrom.xyz>
2025-08-11 19:55:39 +0900

    refactor alignment debug a bit more

--- src/alignment.rs
+++ src/alignment.rs
@@ -16,11 +16,8 @@ use crate::spatial::world_size_for_height;
 use crate::terrain;
 
 const MAX_TURNS: usize = 8;
-const GEOMETRY_DEBUG: u8 = 1; // Levels 0, 1, 2
-
-/// Gizmo configuration for alignment path visualization
-#[derive(Default, Reflect, GizmoConfigGroup)]
-pub struct AlignmentGizmos;
+const FRAC_PI_180: f64 = PI / 180.;
+const MAX_GEOMETRY_DEBUG_LEVEL: u8 = 2;
 
 pub struct AlignmentPlugin;
 
@@ -28,6 +25,7 @@ impl Plugin for AlignmentPlugin {
 	fn build(&self, app: &mut App) {
 		app
 			.insert_resource(load_alignment())
+			.insert_resource(GeometryDebugLevel(MAX_GEOMETRY_DEBUG_LEVEL))
 			// .insert_resource(init_alignments())
 			.init_gizmo_group::<AlignmentGizmos>()
 			.add_systems(Startup, (startup, configure_gizmos))
@@ -71,6 +69,10 @@ impl AlignmentPoint {
 		})
 	}
 }
+
+/// Gizmo configuration for alignment path visualization
+#[derive(Default, Reflect, GizmoConfigGroup)]
+pub struct AlignmentGizmos;
 
 fn configure_gizmos(mut config_store: ResMut<GizmoConfigStore>) {
 	let (config, _) = config_store.config_mut::<AlignmentGizmos>();
@@ -199,17 +201,30 @@ fn update_alignment_from_intermediate_pins(
 	}
 }
 
+#[derive(Resource)]
+struct GeometryDebugLevel(u8);
+
 fn ui(
 	mut contexts: EguiContexts,
 	mut alignment_state: ResMut<AlignmentState>,
+	mut path_debug_level: ResMut<GeometryDebugLevel>,
 	alignment_pins: Query<(&Transform, &AlignmentPoint)>,
 ) {
+	let path_debug_level = &mut path_debug_level.0;
 	if let Ok(ctx) = contexts.ctx_mut() {
 		egui::Window::new("Alignment Properties")
 			.default_pos((00.0, 35.0))
 			.default_open(false)
 			.show(ctx, |ui| {
-				// Debug: show current alignment and pin count
+				ui.label(format!("Path Debug Level: {path_debug_level}"));
+				ui.horizontal(|ui| {
+					for i in 0..=MAX_GEOMETRY_DEBUG_LEVEL {
+						if ui.button(i.to_string()).clicked() {
+							*path_debug_level = i;
+						};
+					}
+				});
+
 				ui.label(format!("Total turns: {}", alignment_state.turns));
 				ui.label(format!("Total pins: {}", alignment_pins.iter().count()));
 
@@ -309,9 +324,11 @@ fn vertex_properties_ui(ui: &mut egui::Ui, alignment_state: &mut AlignmentState)
 					ui.add(
 						egui::Slider::new(
 							&mut segment.circular_section_angle,
-							0.0..=std::f32::consts::FRAC_PI_2,
+							0.0..=std::f32::consts::TAU,
 						)
-						.custom_formatter(|val, _| format!("{:.2?}°", val.to_degrees())),
+						.step_by(FRAC_PI_180) // 1 degree
+						.custom_parser(|s| s.parse::<f64>().ok().map(|f| f.to_radians()))
+						.custom_formatter(|val, _| format!("{:.0?}°", val.to_degrees())),
 					);
 					ui.end_row();
 					ui.label("Radius:");
@@ -545,11 +562,16 @@ fn update_pins_from_alignment_state(
 	}
 }
 
+const CURVE_RESOLUTION: u32 = 16;
+
 fn render_alignment_path(
 	mut gizmos: Gizmos<AlignmentGizmos>,
 	alignment_state: Res<AlignmentState>,
 	alignment_pins: Query<(&Transform, &AlignmentPoint)>,
+	geometry_debug_level: Res<GeometryDebugLevel>,
 ) {
+	let geometry_debug_level = geometry_debug_level.0;
+
 	let (start, end) = match get_start_and_end_points(&alignment_state, alignment_pins) {
 		Some(value) => value,
 		None => return,
@@ -559,212 +581,218 @@ fn render_alignment_path(
 		let segments = add_start_and_end_pins(start, end, alignment);
 		let mut c_i_minus_1 = None;
 		for i in 0..segments.len() - 1 {
-			{
-				let gizmos: &mut Gizmos<'_, '_, AlignmentGizmos> = &mut gizmos;
-				let segments: &[PathSegment] = &segments;
-				let segment_i = segments[i];
-				let segment_i_plus_1 = segments[i + 1];
-				let tangent_vertex_i = segment_i.tangent_vertex;
-				let tangent_vertex_i_plus_1 = segment_i_plus_1.tangent_vertex;
-				let circular_section_radius_i = segment_i.circular_section_radius;
-				let circular_section_angle_i = segment_i.circular_section_angle;
-				let clothoid_curve_iterations = 32;
-				let unit_vector_i_plus_1 = unit_vector(tangent_vertex_i_plus_1, tangent_vertex_i);
+			let gizmos: &mut Gizmos<'_, '_, AlignmentGizmos> = &mut gizmos;
+			let segments: &[PathSegment] = &segments;
+			let segment_i = segments[i];
+			let segment_i_plus_1 = segments[i + 1];
+			let tangent_vertex_i = segment_i.tangent_vertex;
+			let tangent_vertex_i_plus_1 = segment_i_plus_1.tangent_vertex;
+			let circular_section_radius_i = segment_i.circular_section_radius;
+			let circular_section_angle_i = segment_i.circular_section_angle;
+			let unit_vector_i_plus_1 = unit_vector(tangent_vertex_i_plus_1, tangent_vertex_i);
 
-				if i > 0 {
-					let segment_i_minus_1 = segments[i - 1];
-					let tangent_vertex_i_minus_1 = segment_i_minus_1.tangent_vertex;
-					let azimuth_of_tangent_i = azimuth_of_tangent(tangent_vertex_i, tangent_vertex_i_minus_1);
-					let azimuth_of_tangent_i_plus_1 =
-						azimuth_of_tangent(tangent_vertex_i_plus_1, tangent_vertex_i);
-					let difference_in_azimuth_i =
-						difference_in_azimuth(azimuth_of_tangent_i, azimuth_of_tangent_i_plus_1);
-					// Ensure theta_i is positive and handle angle wrapping
-					let length_of_circular_section = circular_section_length(
-						circular_section_radius_i,
-						circular_section_angle_i,
+			if i > 0 {
+				let segment_i_minus_1 = segments[i - 1];
+				let tangent_vertex_i_minus_1 = segment_i_minus_1.tangent_vertex;
+				let azimuth_of_tangent_i = azimuth_of_tangent(tangent_vertex_i, tangent_vertex_i_minus_1);
+				let azimuth_of_tangent_i_plus_1 =
+					azimuth_of_tangent(tangent_vertex_i_plus_1, tangent_vertex_i);
+				let difference_in_azimuth_i =
+					difference_in_azimuth(azimuth_of_tangent_i, azimuth_of_tangent_i_plus_1);
+				// Ensure theta_i is positive and handle angle wrapping
+				let length_of_circular_section = circular_section_length(
+					circular_section_radius_i,
+					circular_section_angle_i,
+					difference_in_azimuth_i,
+				);
+
+				let unit_vector_i = unit_vector(tangent_vertex_i, tangent_vertex_i_minus_1);
+
+				if geometry_debug_level >= 2 {
+					// azimuth arc
+					gizmos.arc_3d(
+						azimuth_of_tangent_i,
+						150.0, // radius
+						Isometry3d::new(
+							segments[i].tangent_vertex,
+							Quat::from_axis_angle(Vec3::Y, 0.),
+						),
+						Color::srgb(0.9, 1.0, 0.2), // yellow
+					);
+
+					// azimuth reference line
+					gizmos.line(
+						tangent_vertex_i,
+						tangent_vertex_i + Vec3::ZERO.with_x(175.0),
+						Color::srgb(1.0, 0.8, 0.4), // orange
+					);
+
+					// difference in azimuths
+					gizmos.arc_3d(
 						difference_in_azimuth_i,
+						200.0, // radius
+						Isometry3d::new(
+							segments[i].tangent_vertex,
+							Quat::from_axis_angle(Vec3::Y, azimuth_of_tangent_i),
+						),
+						Color::srgb(0.6, 0.0, 1.0), // purple
 					);
+				}
 
-					let unit_vector_i = unit_vector(tangent_vertex_i, tangent_vertex_i_minus_1);
+				// ------ BEGIN IN-JUNCTION CALCULATION ------
+				let total_tangent_length_i = total_tangent_length(
+					circular_section_radius_i,
+					circular_section_angle_i,
+					difference_in_azimuth_i,
+					length_of_circular_section,
+				);
 
-					if GEOMETRY_DEBUG >= 2 {
-						// azimuth arc
-						gizmos.arc_3d(
-							azimuth_of_tangent_i,
-							150.0, // radius
-							Isometry3d::new(
-								segments[i].tangent_vertex,
-								Quat::from_axis_angle(Vec3::Y, 0.),
-							),
-							Color::srgb(0.9, 1.0, 0.2), // yellow
-						);
+				let t_i = tangent_vertex_i - total_tangent_length_i * unit_vector_i;
 
-						// azimuth reference line
-						gizmos.line(
-							tangent_vertex_i,
-							tangent_vertex_i + Vec3::ZERO.with_x(175.0),
-							Color::srgb(1.0, 0.8, 0.4), // orange
-						);
-
-						// difference in azimuths
-						gizmos.arc_3d(
-							difference_in_azimuth_i,
-							200.0, // radius
-							Isometry3d::new(
-								segments[i].tangent_vertex,
-								Quat::from_axis_angle(Vec3::Y, azimuth_of_tangent_i),
-							),
-							Color::srgb(0.6, 0.0, 1.0), // purple
-						);
-					}
-
-					// ------ BEGIN IN-JUNCTION CALCULATION ------
-					let total_tangent_length_i = total_tangent_length(
-						circular_section_radius_i,
-						circular_section_angle_i,
-						difference_in_azimuth_i,
-						length_of_circular_section,
+				if geometry_debug_level >= 2 {
+					gizmos.sphere(
+						Isometry3d::from_translation(t_i),
+						10.0,
+						Color::srgb(1.0, 1.0, 0.0),
 					);
+				}
+				// ------ END IN-JUNCTION CALCULATION ------
 
-					let t_i = tangent_vertex_i - total_tangent_length_i * unit_vector_i;
+				// ------ BEGIN IN-CLOTHOID CALCULATION ------
+				// Calculate clothoid parameters
+				let r_i_abs = f64::from(circular_section_radius_i.abs());
+				let l_c_abs = f64::from(length_of_circular_section.abs());
 
-					if GEOMETRY_DEBUG >= 2 {
-						gizmos.sphere(
-							Isometry3d::from_translation(t_i),
-							10.0,
-							Color::srgb(1.0, 1.0, 0.0),
-						);
-					}
-					// ------ END IN-JUNCTION CALCULATION ------
+				// Cross product Y component for x-z plane to determine orientation
+				let cross_y = unit_vector_i.x.mul_add(
+					unit_vector_i_plus_1.z,
+					-(unit_vector_i.z * unit_vector_i_plus_1.x),
+				);
+				let lambda_i = if cross_y >= 0.0 { 1.0_f64 } else { -1.0_f64 };
 
-					// ------ BEGIN IN-CLOTHOID CALCULATION ------
-					// Calculate clothoid parameters
-					let r_i_abs = f64::from(circular_section_radius_i.abs());
-					let l_c_abs = f64::from(length_of_circular_section.abs());
+				let inner = (PI * r_i_abs * l_c_abs) / lambda_i;
+				let fresnel_scale = inner.abs().sqrt();
+				let fresnel_scale_sign = inner.signum();
 
-					// Cross product Y component for x-z plane to determine orientation
-					let cross_y = unit_vector_i.x.mul_add(
-						unit_vector_i_plus_1.z,
-						-(unit_vector_i.z * unit_vector_i_plus_1.x),
+				let ingoing_clothoid = FunctionCurve::new(Interval::UNIT, |s| {
+					clothoid_point(
+						f64::from(s),
+						t_i,
+						l_c_abs,
+						f64::from(unit_vector_i.z.atan2(unit_vector_i.x)), // beta_i
+						fresnel_scale,
+						fresnel_scale_sign,
+					)
+				});
+
+				if geometry_debug_level >= 1 {
+					gizmos.curve_3d(
+						ingoing_clothoid,
+						(0..=CURVE_RESOLUTION).map(|i| i as f32 / CURVE_RESOLUTION as f32),
+						MAGENTA,
 					);
-					let lambda_i = if cross_y >= 0.0 { 1.0_f64 } else { -1.0_f64 };
+				}
+				// ------ END IN-CLOTHOID CALCULATION ------
 
-					let beta_i = f64::from(unit_vector_i.z.atan2(unit_vector_i.x));
+				// ------ BEGIN CIRCULAR ARC CALCULATION ------
+				let f_i = f_i(
+					t_i,
+					l_c_abs,
+					f64::from(unit_vector_i.z.atan2(unit_vector_i.x)),
+					fresnel_scale,
+					fresnel_scale_sign,
+				);
+				let clothoid_angle_change =
+					lambda_i as f32 * (difference_in_azimuth_i - circular_section_angle_i) / 2.0;
+				let clothoid_end_tangent_angle =
+					unit_vector_i.z.atan2(unit_vector_i.x) + clothoid_angle_change;
 
-					let inner = (PI * r_i_abs * l_c_abs) / lambda_i;
-					let fresnel_scale = inner.abs().sqrt();
-					let fresnel_scale_sign = inner.signum();
+				let w_i = w_i(lambda_i as f32, clothoid_end_tangent_angle);
+				let o_i = o_i(circular_section_radius_i, f_i, w_i);
+				let start_vector = f_i - o_i;
+				let alpha_i = alpha_i(start_vector);
 
-					let ingoing_clothoid = FunctionCurve::new(Interval::UNIT, |s| {
-						let tilde_s = f64::from(s) * l_c_abs;
-
-						let fresnel_arg = tilde_s / fresnel_scale;
-
-						let fresnel = fresnel_arg.fresnel();
-
-						let i_x = (fresnel_scale
-							* ((beta_i * fresnel_scale_sign).cos() * fresnel.c
-								- (beta_i * fresnel_scale_sign).sin() * fresnel.s)) as f32;
-						let i_z = (fresnel_scale_sign
-							* fresnel_scale
-							* ((beta_i * fresnel_scale_sign).sin() * fresnel.c
-								+ (beta_i * fresnel_scale_sign).cos() * fresnel.s)) as f32;
-
-						t_i + Vec3::new(i_x, 0.0, i_z)
-					});
-
-					if GEOMETRY_DEBUG >= 1 {
-						gizmos.curve_3d(
-							ingoing_clothoid,
-							(0..=clothoid_curve_iterations).map(|i| i as f32 / clothoid_curve_iterations as f32),
-							LIGHT_GREEN,
-						);
-					}
-					// ------ END IN-CLOTHOID CALCULATION ------
-
-					// ------ BEGIN CIRCULAR ARC CALCULATION ------
-					let f_i = f_i(t_i, l_c_abs, beta_i, fresnel_scale, fresnel_scale_sign);
-					// At the end of the ingoing clothoid, the tangent has rotated by half the total
-					// turn angle
-					let clothoid_angle_change = lambda_i
-						* (f64::from(difference_in_azimuth_i) - f64::from(circular_section_angle_i))
-						/ 2.0;
-					let clothoid_end_tangent_angle = beta_i + clothoid_angle_change;
-
-					let w_i = w_i(lambda_i, clothoid_end_tangent_angle);
-					let o_i = o_i(circular_section_radius_i, f_i, w_i);
-					let start_vector = f_i - o_i;
-					let alpha_i = alpha_i(start_vector);
-
-					let arc_sweep = -lambda_i.signum() as f32 * circular_section_angle_i;
-					if GEOMETRY_DEBUG >= 1 {
-						gizmos.arc_3d(
+				let arc_sweep = -lambda_i.signum() as f32 * circular_section_angle_i;
+				if geometry_debug_level >= 1 {
+					gizmos
+						.arc_3d(
 							arc_sweep,
 							circular_section_radius_i,
 							Isometry3d::new(o_i, Quat::from_axis_angle(Vec3::Y, -alpha_i)),
 							RED,
-						);
+						)
+						.resolution(CURVE_RESOLUTION);
+				}
+
+				if geometry_debug_level >= 2 {
+					gizmos.sphere(Isometry3d::from_translation(f_i), 8.0, YELLOW);
+				}
+				// ------ END CIRCULAR ARC CALCULATION ------
+
+				// ------ BEGIN OUT-JUNCTION CALCULATION ------
+				let c_i = tangent_vertex_i + total_tangent_length_i * unit_vector_i_plus_1;
+
+				if geometry_debug_level >= 2 {
+					gizmos.sphere(Isometry3d::from_translation(c_i), 10.0, YELLOW);
+				}
+				// ------ END OUT-JUNCTION CALCULATION ------
+
+				if geometry_debug_level >= 1 {
+					if let Some(c_i_minus_1) = c_i_minus_1 {
+						gizmos.line(c_i_minus_1, t_i, AQUA);
+					} else {
+						gizmos.line(tangent_vertex_i_minus_1, t_i, AQUA);
 					}
-
-					if GEOMETRY_DEBUG >= 2 {
-						gizmos.sphere(Isometry3d::from_translation(f_i), 8.0, AQUA);
+					if i == segments.len() - 2 {
+						gizmos.line(c_i, tangent_vertex_i_plus_1, AQUA);
 					}
-					// ------ END CIRCULAR ARC CALCULATION ------
+				}
+				c_i_minus_1 = Some(c_i);
 
-					// ------ BEGIN OUT-JUNCTION CALCULATION ------
-					let c_i = tangent_vertex_i + total_tangent_length_i * unit_vector_i_plus_1;
+				// ------ BEGIN OUT-CLOTHOID CALCULATION ------
+				let outgoing_clothoid = FunctionCurve::new(Interval::UNIT, |s| {
+					clothoid_point(
+						f64::from(-s),
+						c_i,
+						l_c_abs,
+						f64::from(unit_vector_i_plus_1.z.atan2(unit_vector_i_plus_1.x)),
+						fresnel_scale,
+						-fresnel_scale_sign,
+					)
+				});
 
-					if GEOMETRY_DEBUG >= 2 {
-						gizmos.sphere(Isometry3d::from_translation(c_i), 10.0, YELLOW);
-					}
-					// ------ END OUT-JUNCTION CALCULATION ------
-
-					if GEOMETRY_DEBUG >= 1 {
-						if let Some(c_i_minus_1) = c_i_minus_1 {
-							gizmos.line(c_i_minus_1, t_i, OLIVE);
-						} else {
-							gizmos.line(tangent_vertex_i_minus_1, t_i, AQUA);
-						}
-						if i == segments.len() - 2 {
-							gizmos.line(c_i, tangent_vertex_i_plus_1, GREEN);
-						}
-					}
-					c_i_minus_1 = Some(c_i);
-
-					// ------ BEGIN OUT-CLOTHOID CALCULATION ------
-					// For outgoing clothoid, beta should be angle between u_{i+1} and OX+
-					let beta_i_plus_1 = f64::from(unit_vector_i_plus_1.z.atan2(unit_vector_i_plus_1.x));
-					let outgoing_clothoid = FunctionCurve::new(Interval::UNIT, |s| {
-						let tilde_s = f64::from(-s) * l_c_abs;
-
-						let fresnel_arg = tilde_s / fresnel_scale;
-
-						let fresnel = fresnel_arg.fresnel();
-
-						let i_x = (fresnel_scale
-							* ((beta_i_plus_1 * fresnel_scale_sign).cos() * fresnel.c
-								+ (beta_i_plus_1 * fresnel_scale_sign).sin() * fresnel.s)) as f32;
-						let i_z = (fresnel_scale_sign
-							* fresnel_scale
-							* ((beta_i_plus_1 * fresnel_scale_sign).sin() * fresnel.c
-								- (beta_i_plus_1 * fresnel_scale_sign).cos() * fresnel.s)) as f32;
-
-						c_i + Vec3::new(i_x, 0.0, i_z)
-					});
-
-					if GEOMETRY_DEBUG >= 1 {
-						gizmos.curve_3d(
-							outgoing_clothoid,
-							(0..=clothoid_curve_iterations).map(|i| i as f32 / clothoid_curve_iterations as f32),
-							LIGHT_GREEN,
-						);
-					}
-					// ------ END OUT-CLOTHOID CALCULATION ------
-				};
+				if geometry_debug_level >= 1 {
+					gizmos.curve_3d(
+						outgoing_clothoid,
+						(0..=CURVE_RESOLUTION).map(|i| i as f32 / CURVE_RESOLUTION as f32),
+						MAGENTA,
+					);
+				}
+				// ------ END OUT-CLOTHOID CALCULATION ------
 			};
 		}
 	};
+}
+
+fn clothoid_point(
+	s: f64,
+	clothoid_endpoint: Vec3,
+	l_c_abs: f64,
+	beta_i: f64,
+	fresnel_scale: f64,
+	fresnel_scale_sign: f64,
+) -> Vec3 {
+	let tilde_s = s * l_c_abs;
+	let fresnel_arg = tilde_s / fresnel_scale;
+	let fresnel = fresnel_arg.fresnel();
+	let i_x = (fresnel_scale
+		* ((beta_i * fresnel_scale_sign).cos() * fresnel.c
+			- (beta_i * fresnel_scale_sign).sin() * fresnel.s)) as f32;
+	let i_z = (fresnel_scale_sign
+		* fresnel_scale
+		* ((beta_i * fresnel_scale_sign).sin() * fresnel.c
+			+ (beta_i * fresnel_scale_sign).cos() * fresnel.s)) as f32;
+	clothoid_endpoint + Vec3::new(i_x, 0.0, i_z)
 }
 
 fn add_start_and_end_pins(start: Vec3, end: Vec3, alignment: &Alignment) -> Vec<PathSegment> {
@@ -792,20 +820,20 @@ fn o_i(circular_section_radius_i: f32, f_i: Vec3, w_i: Vec3) -> Vec3 {
 	f_i + circular_section_radius_i * w_i
 }
 
-fn w_i(lambda_i: f64, clothoid_end_tangent_angle: f64) -> Vec3 {
+fn w_i(lambda_i: f32, clothoid_end_tangent_angle: f32) -> Vec3 {
 	if lambda_i > 0.0 {
 		// Left turn: rotate tangent direction 90° counter-clockwise
 		Vec3::new(
-			-(clothoid_end_tangent_angle.sin() as f32),
+			-(clothoid_end_tangent_angle.sin()),
 			0.0,
-			clothoid_end_tangent_angle.cos() as f32,
+			clothoid_end_tangent_angle.cos(),
 		)
 	} else {
 		// Right turn: rotate tangent direction 90° clockwise
 		Vec3::new(
-			clothoid_end_tangent_angle.sin() as f32,
+			clothoid_end_tangent_angle.sin(),
 			0.0,
-			-(clothoid_end_tangent_angle.cos() as f32),
+			-(clothoid_end_tangent_angle.cos()),
 		)
 	}
 }
