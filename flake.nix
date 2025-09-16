97fe80fac302b351728262bb39b55f8c8b075ec5
Christian Westrom <c.westrom@westrom.xyz>
2025-09-17 00:21:19 +0900

    update dependencies

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Rust example flake for Zero to Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     systems.url = "github:nix-systems/default";
   };
